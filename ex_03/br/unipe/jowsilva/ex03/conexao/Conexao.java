7a3eb85447424fa683b33ecd623e3da129571977
jowsilva <jowsilva@W114CPD001.br-atacadao.corp>
2016-08-17 15:56:53 -0300

    Finalizado exercício com Singleton.

--- ex_03/br/unipe/jowsilva/ex03/conexao/Conexao.java
+++ ex_03/br/unipe/jowsilva/ex03/conexao/Conexao.java
@@ -2,4 +2,20 @@ package br.unipe.jowsilva.ex03.conexao;
 
 public class Conexao {
 
+	private static Conexao instancia;
+
+	private Conexao() {
+
+	}
+
+	public static synchronized Conexao getInstance(){
+		if (instancia == null){
+			instancia = new Conexao();
+		}else{
+			System.out.println("");
+			System.out.println("Já existe uma conexão em andamento.");
+			System.out.println("");
+		}
+		return instancia;
+	}
 }
--- ex_03/br/unipe/jowsilva/ex03/ui/Main.java
+++ ex_03/br/unipe/jowsilva/ex03/ui/Main.java
@@ -1,5 +1,39 @@
 package br.unipe.jowsilva.ex03.ui;
 
+import java.util.Scanner;
+
+import br.unipe.jowsilva.ex03.conexao.Conexao;
+
 public class Main {
 
+	public static void main(String[] args) {
+
+		int opcao = -1;
+
+		Scanner leitor = new Scanner(System.in);
+
+		while (opcao != 2) {
+
+			System.out.println("Deseja realizar uma conexão?");
+
+			System.out.println("1 - Sim");
+			System.out.println("2 - Não");
+
+			opcao = leitor.nextInt();
+
+			switch (opcao) {
+
+			case 1:
+				Conexao.getInstance();
+				break;
+			case 2:
+				System.out.println("Operação Cancelada");
+
+			}
+
+		}
+
+		leitor.close();
+
+	}
 }
